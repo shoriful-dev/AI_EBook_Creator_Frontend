5967dd1424b6b1969f5adeebbd04df5222654a84
shoriful-dev <cmshohelrana50@gmail.com>
2026-02-06 13:35:41 +0600

    add ProfileDropdown Menu

--- src/components/auth/ProtectedRoute.jsx
+++ src/components/auth/ProtectedRoute.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { Loader } from 'lucide-react';
+import { useAuth } from '../../context/AuthContext';
 
 const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = false;
-  const loading = false;
+  const {isAuthenticated, loading} = useAuth();
   const location = useLocation();
 
   if (loading) {
