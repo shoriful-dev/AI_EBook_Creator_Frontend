a2e51955fa4df6324760b802ce4bb277a24f6c31
shoriful-dev <cmshohelrana50@gmail.com>
2026-02-07 20:35:22 +0600

    create Dropdown Menu

--- src/components/ui/Dropdown.jsx
+++ src/components/ui/Dropdown.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
-const Dropdown = () => {
-  return <div>Dropdown</div>;
+const Dropdown = ({ trigger, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if(dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    }
+
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    }
+  },[])
+  return (
+    <div className="relative inline-block text-left" ref={dropdownRef}>
+      <div onClick={() => setIsOpen(!isOpen)}>{trigger}</div>
+
+      {isOpen && (
+        <div className="absolute right-0 mt-2 z-20 w-56 origin-top-right rounded-lg shadow-lg bg-white border border-slate-200 focus:outline-none" role='menu' aria-orientation='vertical' aria-labelledby='menu-button' tabIndex='-1'>
+          <div className="py-1" role='none'>
+            {children}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export const DropdownItem = ({ children, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="flex items-center w-full px-4 py-2 text-sm text-slate-700 hover:bg-slate-100 text-left cursor-pointer"
+      role="menuitem"
+      tabIndex="-1"
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Dropdown;
--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -1,4 +1,16 @@
 import React from 'react';
+import { useEffect, useState, useRef } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import toast from 'react-hot-toast';
+import { Sparkles, FileDown, Save, Menu, X, Edit, NotebookText, ChevronDown, FileText } from 'lucide-react';
+import { arrayMove } from '@dnd-kit/sortable';
+import axiosInstance from '../utils/axiosInstance';
+import { API_PATHS } from '../utils/apiPaths';
+import Dropdown, {DropdownItem} from '../components/ui/Dropdown';
+import InputField from '../components/ui/InputField';
+import Button from '../components/ui/Button';
+import Modal from '../components/ui/Modal';
+import SelectField from '../components/ui/SelectField';
 
 const EditorPage = () => {
   return <div>EditorPage</div>;
