e997fc2706be0eeb5e0d18841a6e538cbdf0bc10
shoriful-dev <cmshohelrana50@gmail.com>
2026-02-09 22:04:03 +0600

    add build configuration for chunking in Vite

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          utils: ['axios', 'lucide-react', 'react-hot-toast'],
+          dnd: ['@dnd-kit/core', '@dnd-kit/sortable'],
+          editor: ['@uiw/react-md-editor'],
+        }
+      }
+    }
+  }
 });
